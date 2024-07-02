cb8cf9857d4d9d43ca384b77ef77ccad7582cc93
AkilaInduwara <akilainduwara@gmail.com>
2024-07-02 22:40:41 +0530

    nav bar part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../src/CSS/App.css';
 import './CSS/poppins.css'
+import Header from './Components/Header/Header';
+
 
 function App() {
   return (
    <>
-   <h1>akakak</h1>
+   <Header />
    </>
   );
 }
